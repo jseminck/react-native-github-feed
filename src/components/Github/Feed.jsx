356a7071a95b0541f003dd1c7ffde2877f1f30f2
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-06-11 23:33:54 +0300

    Add a basic ListView

--- src/components/Github/Feed.jsx
+++ src/components/Github/Feed.jsx
@@ -1,16 +1,40 @@
-import React, { Text, View } from 'react-native';
+import React, { Text, View, ListView } from 'react-native';
 
 class Feed extends React.Component {
     static propTypes = {
     }
 
+    constructor(props) {
+        super(props);
+
+        const dataSource = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.state = {
+            dataSource: dataSource.cloneWithRows(['a', 'b', 'c'])
+        };
+    }
+
     render() {
         return (
-            <View>
-                <Text>
-                    Github Feed
-                </Text>
+            <View style={{
+                flex: 1,
+                justifyContent: 'flex-start'
+            }}>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={::this.renderRow}
+                />
             </View>
+        );
+    }
+
+    renderRow(rowData) {
+        return (
+            <Text>
+                {rowData}
+            </Text>
         );
     }
 }
