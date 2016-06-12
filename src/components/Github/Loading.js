fb75ee77c5d5b825275c821fffd5edbedb7d95a9
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-06-12 23:58:16 +0300

    Update the loading screen with an ActivityIndicator

--- src/components/Github/Loading.js
+++ src/components/Github/Loading.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicatorIOS } from 'react-native';
 
 export default () => {
     return (
         <View style={styles.container}>
             <Text>
-                Logout ...
+                Loading
             </Text>
+            <ActivityIndicatorIOS
+                animating={true}
+                size='large'
+                style={styles.activityIndicator}
+            />
         </View>
     );
 };
@@ -16,5 +21,8 @@ const styles = {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    activityIndicator: {
+        marginTop: 15
     }
 };
